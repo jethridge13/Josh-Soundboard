580ac60c117cae1871ce4f8839e471f93299265c
mfrancis95 <mikefrancis95@gmail.com>
2014-12-18 09:55:08 -0500

    Added three more test sounds.

--- js/soundboard.js
+++ js/soundboard.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
     $("li").click(function() {
-        var audio = $("audio");
-        audio.trigger("load");
-        audio.trigger("play");
+        $("audio").trigger("load").eq($(this).index() % 4).trigger("play");
     });
 });
