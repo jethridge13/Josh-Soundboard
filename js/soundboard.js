bfca5ac615da50d58f16eebd56018f93db66011d
mfrancis95 <mikefrancis95@gmail.com>
2014-12-18 11:37:36 -0500

    Started work on an AudioQueue class.

--- js/soundboard.js
+++ js/soundboard.js
@@ -3,3 +3,24 @@ $(document).ready(function() {
         $("audio").trigger("load").eq($(this).index() % 4).trigger("play");
     });
 });
+
+function AudioQueue() {
+    
+    var self = this;
+    
+    var list = [];
+    
+    this.addAudio = function(audio) {
+        list.push(audio);
+    };
+    
+    this.play = function() {
+        var audio = list.shift();
+        if (audio !== null) {
+            audio.addEventListener("ended", self.play);
+            audio.load();
+            audio.play();
+        }
+    };
+    
+}
